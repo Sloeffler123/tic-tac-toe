c6adb4bbb8d6f3e974899592478d174705af361c
Sloeffler123 <samloeffler123@gmail.com>
2025-04-13 12:43:48 -0500

    more features

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from setup import *
-board = [
-    [7,8,9],
-    [4,5,6],
-    [1,2,3],
-]
+
 def main():
-    starting_board(board)
     starting_rules()
     print('\n')
     user_input = type_of_play()
     name1,name2 = players_name(user_input)
+    first,second = sides(name1,name2,user_input)
     print(f'{name1} vs. {name2}')
+    on = True
+    while on:
+        position_x = player_position(name1,board)
+        change_board(position_x,first)
+        position_o = player_position(name2,board)
+        change_board(position_o,second)
+        
     #this is where we need to figure out how to 
     #make player vs player ai vs ai and ai vs player
 
 main()    
+
--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 
+board = [
+    [7,8,9],
+    [4,5,6],
+    [1,2,3],
+]
 
 #board[0][wherever they want to play]
 #input is 1-9 
@@ -10,7 +15,7 @@ def starting_board(board):
             {board[1][0]} | {board[1][1]} | {board[1][2]}
             -----------
             {board[2][0]} | {board[2][1]} | {board[2][2]}'''
-    
+    print(board)
 
 def starting_rules():
     print('This is what the board will look like')
@@ -49,18 +54,56 @@ def players_name(user_input):
         computer_2 = 'Computer 2'
         return computer_1,computer_2
 
-def player_position(player):
-    user_input = int(input(f'{player}, choose your position'))
+def player_position(player,board):
+    starting_board(board)
+    user_input = int(input(f'{player}, choose your position '))
     return user_input
 
 #if both players are none then assign both to be AI if one is absent assign player 2 as AI
-def player_sides(player_1=None, player_2=None):
-    if player_1 is None and player_2 is None:
+def sides(player1,player2,user_input):
+    match user_input:
+        case 0:
+            side = input(f"{player1}, what side would you like to be on? O's or X's")
+            if side.isalpha():
+                if side.upper() == 'O':
+                    first = 'O'
+                    second = 'X'
+                    return first,second
+            else:
+                raise Exception('Please enter a valid input')        
+        case 1:
+            first = 'X'
+            second = 'O'
+            return first,second
+        case 2:
+            first = 'X'
+            second = 'O'
+            return first,second
+        
+def change_board(player_position,player_side):  
+    if player_position in board[2]:
+        index = board[2].index(player_position)
+        board[2][index] = player_side
+    elif player_position in board[1]:
+        index = board[1].index(player_position)
+        board[1][index] = player_side
+    elif player_position in board[0]:
+        index = board[0].index(player_position)
+        board[0][index] = player_side      
+def win_condition(board,):
+    if all(board[0]):
         pass
-
-def change_board(player_position,player_side):
-    position = board[0][player_position]
-    board.remove(position)
-    new_pos = board.insert(position,player_side)
-    starting_board(new_pos)
-    
+    elif all(board[1]):
+        pass
+    elif all(board[2]):
+        pass
+    elif all(board[0][0],board[1][1],board[2][2]):
+        pass
+    elif all(board[0][2],board[1][1],board[2][0]):
+        pass
+    elif all(board[0][0],board[1][0],board[2][0]):
+        pass
+    elif all(board[0][2],board[1][2],board[2][2]):
+        pass
+    elif all(board[0][1],board[1][1],board[2][1]):
+        pass
